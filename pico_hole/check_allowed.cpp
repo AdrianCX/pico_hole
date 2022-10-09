210468f14e365a4ba498f4d033cea23133cd24d6
Adrian Cruceru <cruceruadrian@gmail.com>
2022-10-09 17:25:35 +0200

    Add amazon prime video whitelist

--- pico_hole/check_allowed.cpp
+++ pico_hole/check_allowed.cpp
@@ -2,13 +2,13 @@
 #include "pico/stdlib.h"
 
 const char *allowed_patterns[] = {
-    "netflix.com.",
+    "netflix.com.", // Netflix domains below
     "nflxext.com.",
     "nflximg.com.",
     "nflxso.net.",
     "nflxvideo.net.",
     "netflix.net.",
-    "disneyplus.com.",
+    "disneyplus.com.", // Disney+ domains below
     "bamgrid.com.",
     "bam.nr-data.net.",
     "cdn.registerdisney.go.com.",
@@ -37,9 +37,41 @@ const char *allowed_patterns[] = {
     "microsoft.com.",
     "images.ctfassets.net.",
     "time.samsungcloudsolution.com.",
+    "amazon.com.", //  Amazon prime domains below
+    "amazonvideo.com.",
+    "amazon.co.uk.",
+    "d184dfn36gombl.cloudfront.net.",
+    "media-amazon.com.",
+    "ssl-images-amazon.com.",
+    "aiv-cdn.net.",
+    "aiv-delivery.net.",
+    "images-amazon.com.",
+    "pv-cdn.net.",
 };
 
+// wildcard to match blacklist specific to samsung, don't even print reject. (can switch later to a blacklist style if list below covers everything, to gather logs first)
+const char *ignore_print[] = {
+    "samsung",
+    "d7luqmstmj331.cloudfront.net.",
+    ".adgrx.com.",
+    ".sectigo.com.",
+    ".tivuon.tv.",
+    ".demdex.net.",
+};
+
+/*
+For more domains on samsung, so far lots of subdomains for:
+.samsungads.com.
+.samsungcloudsolution.com.
+.samsungcloudsolution.net.
+.samsungelectronics.com.
+.samsungotn.net.
+.samsungotn.net.
+.samsungqbe.com.
+*/
+
 const int allowed_patterns_size = sizeof(allowed_patterns)/sizeof(const char*);
+const int ignore_print_size = sizeof(ignore_print)/sizeof(const char*);
 
 bool ends_with(const char *str, const char *suffix)
 {
@@ -62,6 +94,14 @@ bool ends_with(const char *str, const char *suffix)
 
 extern "C" bool check_dns_name(int id, const char *name)
 {
+    for (int i=0;i<ignore_print_size;++i)
+    {
+        if (strstr(name, ignore_print[i])!=NULL)
+        {
+            return false;
+        }
+    }
+        
     for (int i=0;i<allowed_patterns_size;++i)
     {
         if (ends_with(name, allowed_patterns[i]))
@@ -70,7 +110,6 @@ extern "C" bool check_dns_name(int id, const char *name)
             return true;
         }
     }
-        
     printf("[%d] Rejecting: %s\r\n", id, name);
     return false;
 }
